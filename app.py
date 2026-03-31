d310ca98e4e554df8ef4de5b5eeb3b87cb82833b
Tiger2605 <assongwabolondja@gmail.com>
2026-03-31 02:37:12 +0100

    🔧 FIX: Correction de la syntaxe du Limiter

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ limiter = Limiter(
     app,
     key_func=get_remote_address,
     app=app,
-    default_limits=["200 per day", "50 per hour"],
+    default_limits=["300 per day", "100 per hour"],
     storage_uri="memory://"
 )
 
