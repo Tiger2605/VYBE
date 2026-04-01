badd392962e2f85a788e5486a5b3aa8f974451b7
Tiger2605 <assongwabolondja@gmail.com>
2026-04-01 19:54:23 +0100

    Augmentation de la taille de la colonne password

--- models.py
+++ models.py
@@ -22,7 +22,7 @@ group_members = db.Table('group_members',
 # ---------------------------------------------------------
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     bio = db.Column(db.String(200), default="Salut, je suis sur VIBE AFRICA !")
